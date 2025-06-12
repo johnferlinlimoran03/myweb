d0b957deffededd53c13ddc94c2d40d2a8a6e38c
John <johnferlinlimoran03@gmail.com>
2025-06-12 20:54:58 +0800

    commit navigationbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import { Drawer, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Box,
+  IconButton,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link, useLocation } from 'react-router-dom';
+
 
 const drawerWidth = 240;
 
@@ -15,39 +29,30 @@ const menuItems = [
 ];
 
 const NavigationBar = () => {
-  const location = useLocation();
-
-  return (
-    <Drawer
-      variant="permanent"
-      anchor="left"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: {
-          width: drawerWidth,
-          boxSizing: 'border-box',
-          backgroundColor: '#0D47A1', // Deep blue
-          color: '#fff'
-        }
-      }}
-    >
+   const location = useLocation();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+const drawerContent = (
+    <>
       <Toolbar>
-        <Typography variant="h6" sx={{ color: '#FFA000' }}> {/* Vivid orange */}
+        <Typography variant="h6" sx={{ color: '#FFA000' }}>
           Purchasing System
         </Typography>
       </Toolbar>
       <Box sx={{ overflow: 'auto' }}>
         <List>
-          {menuItems.map(item => (
+          {menuItems.map((item) => (
             <ListItem key={item.name} disablePadding>
               <ListItemButton
                 component={Link}
                 to={item.path}
                 selected={location.pathname === item.path}
+                onClick={() => isMobile && setMobileOpen(false)} // Close on mobile tap
                 sx={{
                   '&.Mui-selected': {
-                    backgroundColor: '#FFA000', // orange highlight
+                    backgroundColor: '#FFA000',
                     color: '#fff'
                   },
                   '&:hover': {
@@ -62,8 +67,49 @@ const NavigationBar = () => {
           ))}
         </List>
       </Box>
-    </Drawer>
+    </>
+  );
+return (
+    <>
+      {isMobile && (
+        <Toolbar sx={{ backgroundColor: '#0D47A1', color: '#fff' }}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ color: '#FFA000' }}>
+            Purchasing System
+          </Typography>
+        </Toolbar>
+      )}
+
+      <Drawer
+        variant={isMobile ? 'temporary' : 'permanent'}
+        open={isMobile ? mobileOpen : true}
+        onClose={handleDrawerToggle}
+        anchor="left"
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            backgroundColor: '#0D47A1',
+            color: '#fff'
+          }
+        }}
+      >
+        {drawerContent}
+      </Drawer>
+    </>
   );
 };
 
 export default NavigationBar;
+
+
